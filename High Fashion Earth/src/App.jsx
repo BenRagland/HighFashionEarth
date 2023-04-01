8973bf1ab41dbc9b4a5485422ad09ae909db0da8
Bandplay <Officialbenragland@gmail.com>
2023-04-01 18:56:19 -0400

    Establishes the High Fashion Earth Web App & folder structure

---
+++ High Fashion Earth/src/App.jsx
@@ -0,0 +1,16 @@
+import react from "react"
+import Styles from './main.css'
+import Header from './components/Header'
+
+
+function App(){
+    return(
+        <div className="page-container">
+            <Header />
+            
+
+        </div>
+    )
+}
+
+export default App

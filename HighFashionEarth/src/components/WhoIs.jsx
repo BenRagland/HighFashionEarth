ac37f25eb1c740c75de5baa53639bb3df8f6e31c
Bandplay <Officialbenragland@gmail.com>
2023-04-30 14:44:10 -0400

    adds styling to who is section- large screens

--- HighFashionEarth/src/components/WhoIs.jsx
+++ HighFashionEarth/src/components/WhoIs.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Styles from '../main.css'
 import SectionTheme from './SectionTheme'
-
+import FashionModels from '../assets/FashionModels.png'
 function WhoIs (){
     return(
         <SectionTheme color="#F7EFED">
             <div className="who-is-component-container" id="whoSection">
                 <div className='who-is-heading'>
-                    <h2>HIGH FASHION EARTH</h2>
+                
                     <h1> WHO IS HIGH FASHION EARTH?</h1>
                 </div>
                 <p className="who-is-p">We’re a local resale brand, in Arlington, VA, 
@@ -15,8 +15,14 @@ function WhoIs (){
                     /environmentally friendly fashion. We specialize 
                     in resale and authentication of a number of 
                     High Quality brands. To keep it short and sweet..
+                    
+                    <p className="who-is-bold">We BUY vintage/ luxury/ and street wear brands. </p> 
                 </p>
-                <p className="who-is-bold">We BUY vintage/ luxury/ and street wear brands. </p> 
+
+                <img 
+                    src={FashionModels}
+                    className="fashion-models"
+                />
             </div>   
         </SectionTheme >
     )
